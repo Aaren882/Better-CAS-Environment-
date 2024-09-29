2ebfec822a4d3b4f4c1426ec74ebccd5c3064bd7
Aaren <aaren.yang@gmail.com>
2024-09-29 14:53:14 +0800

    Fix Server unable to save CBA cTab Settings

--- CBA_Setting.hpp
+++ CBA_Setting.hpp
@@ -121,7 +121,9 @@
 	}
 ] call CBA_fnc_addSetting;
 
-//- Set File type (ATAK)
+#include "\MG8\AVFEVFX\cTab\has_cTab.hpp"
+//- Set File type (cTab/ATAK)
+#ifdef cTAB_Installed
 	[
 		"BCE_PicFile_list", "LIST",
 		[localize "STR_BCE_Select_PIC_FILE"],
@@ -149,7 +151,33 @@
 			Systemchat (_return # 0);
 		}
 	] call CBA_fnc_addSetting;
-
+	//- cTab server side
+	[
+		"BCE_cTab_Side_Display", "CHECKBOX",
+		[localize "STR_BCE_cTab_ATAK_Side_Display",localize "STR_BCE_cTab_ATAK_Side_Display_tip"],
+		["Better CAS Environment (Server)",localize "STR_BCE_cTab_Settings"],
+		true,
+		1
+	] call CBA_fnc_addSetting;
+	[
+		"BCE_cTab_Marker_Sync", "LIST",
+		[localize "STR_BCE_cTab_Marker_Sync",localize "STR_BCE_cTab_Marker_Sync_tip"],
+		["Better CAS Environment (Server)",localize "STR_BCE_cTab_Settings"],
+		[
+			["_IcTab", "_USER"],
+			[localize "STR_MPROLE_DISABLE",localize "STR_OPT_ENABLE"],
+			1
+		],
+		1
+	] call CBA_fnc_addSetting;
+	[
+		"BCE_cTab_Marker_Sync_time", "TIME",
+		[localize "STR_BCE_cTab_Marker_Sync_Time"],
+		["Better CAS Environment (Server)",localize "STR_BCE_cTab_Settings"],
+		[1, 5, 1],
+		1
+	] call CBA_fnc_addSetting;
+#endif
 ///////////////////IR Stuffs//////////////////////
 [
 	"BCE_veh_IR_fn","CHECKBOX",
--- functions/cTab/fn_cTab_postInit.sqf
+++ functions/cTab/fn_cTab_postInit.sqf
@@ -98,31 +98,6 @@ cTab_msg_gui_load = ctab_fnc_msg_gui_load;
 cTabTxtSize = 0.06;
 
 //- CBA Settings
-	[
-		"BCE_cTab_Side_Display", "CHECKBOX",
-		[localize "STR_BCE_cTab_ATAK_Side_Display",localize "STR_BCE_cTab_ATAK_Side_Display_tip"],
-		["Better CAS Environment (Server)",localize "STR_BCE_cTab_Settings"],
-		true,
-		1
-	] call CBA_fnc_addSetting;
-	[
-		"BCE_cTab_Marker_Sync", "LIST",
-		[localize "STR_BCE_cTab_Marker_Sync",localize "STR_BCE_cTab_Marker_Sync_tip"],
-		["Better CAS Environment (Server)",localize "STR_BCE_cTab_Settings"],
-		[
-			["_IcTab", "_USER"],
-			[localize "STR_MPROLE_DISABLE",localize "STR_OPT_ENABLE"],
-			1
-		],
-		1
-	] call CBA_fnc_addSetting;
-	[
-		"BCE_cTab_Marker_Sync_time", "TIME",
-		[localize "STR_BCE_cTab_Marker_Sync_Time"],
-		["Better CAS Environment (Server)",localize "STR_BCE_cTab_Settings"],
-		[1, 5, 1],
-		1
-	] call CBA_fnc_addSetting;
 	BCE_cTab_Marker_Sync_time call BCE_fnc_cTab_Marker_update;
 
 //////////////////////////////////////////////////////////////
