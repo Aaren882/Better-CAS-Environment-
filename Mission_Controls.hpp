097ceef894c2f5d7d2d2f9796736fe8ed19503aa
Aaren <aaren.yang@gmail.com>
2025-03-16 17:03:00 +0800

    Add mission framework properties definition for BCE task builder

---
+++ Mission_Controls.hpp
@@ -0,0 +1,445 @@
+/*
+    This file is used to define the controls used in the mission building.
+
+    [X, Y, W, H] are defined in the UI that uses this Config.
+    [size, SizeEx] Also
+*/
+#define PROP_IDC_OFFSET(IDC) __EVAL(30000 + IDC)
+#define REGISTER_FNC \
+    onLoad = "call BCE_fnc_RegisterMissionControls"
+
+class BCE_Mission_Build_Controls
+{
+    //- Air Mission Type
+    class TaskType: RscCombo
+    {
+        REGISTER_FNC;
+
+        idc = 2107;
+        colorBackground[] = {0,0,0,1};
+        colorSelectBackground[] = {0.5,0.5,0.5,1};
+        wholeHeight = 0.8;
+        class Items
+        {
+            class 9line
+            {
+                text = "9 Line";
+                textRight = "";
+                value = 0;
+                default = 1;
+            };
+            class 5line
+            {
+                text = "5 Line";
+                textRight = "";
+                value = 1;
+            };
+        };
+    };
+    //-Description
+    class taskDesc: RscStructuredText
+    {
+        REGISTER_FNC;
+
+        idc = 2004;
+        text = "Desc :";
+        colorBackground[] = {0,0,0,0};
+        class Attributes
+        {
+            font = "RobotoCondensed_BCE";
+            color = "#ffffff";
+            align = "left";
+            shadow = 1;
+        };
+    };
+    class New_Task_Ctrl_Title: RscButtonMenu
+    {
+        idc = PROP_IDC_OFFSET(20110);
+        style = 2;
+        text = "$STR_BCE_ControlType_BNT";
+        colorBackground[] = {0,0,0,0.4};
+        show = 0;
+        shadow = 1;
+        periodFocus = 0;
+        periodOver = 0;
+        tooltip = "$STR_BCE_more_Details";
+        onButtonClick = "call BCE_fnc_Extended_Desc";
+        BCE_Desc = "$STR_BCE_DECS_sm_CtrlType";
+        class Attributes
+        {
+            font = "RobotoCondensed_BCE";
+            color = "#E5E5E5";
+            align = "left";
+            shadow = "true";
+        };
+    };
+
+    //- Game Plan
+        class New_Task_CtrlType: RscToolbox
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(2011);
+            rows = 1;
+            columns = 3;
+            strings[] =
+            {
+                "Type 1",
+                "Type 2",
+                "Type 3"
+            };
+            font = "RobotoCondensed_BCE";
+            colorBackground[] = {0,0,0,0.3};
+        };
+        class New_Task_AttackType_Title: New_Task_Ctrl_Title
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(20111);
+            text = "$STR_BCE_AttackType_BNT";
+            BCE_Desc = "$STR_BCE_DECS_sm_AttackType";
+        };
+        class New_Task_AttackType: New_Task_CtrlType
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(20112);
+            columns = 2;
+            strings[] =
+            {
+                "BoT",
+                "BoC"
+            };
+        };
+        class New_Task_AttackType_Combo: TaskType
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(20112);
+            columns = 2;
+            class Items
+            {
+                class BoT
+                {
+                    text = "BoT";
+                    textRight = "";
+                    value = 0;
+                    default = 1;
+                };
+                class BoC
+                {
+                    text = "BoC";
+                    textRight = "";
+                    value = 1;
+                };
+            };
+        };
+        class New_Task_Ordnance_Title: New_Task_Ctrl_Title
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(20113);
+            text = "$STR_BCE_OrdnanceREQ_BNT";
+            BCE_Desc = "$STR_BCE_DECS_sm_Ordnance";
+        };
+        //- Weapon
+            class AI_Remark_WeaponCombo: RscCombo
+            {
+                REGISTER_FNC;
+
+                idc = PROP_IDC_OFFSET(2020);
+            };
+            class AI_Remark_ModeCombo: AI_Remark_WeaponCombo
+            {
+                REGISTER_FNC;
+
+                idc = PROP_IDC_OFFSET(2021);
+            };
+            class Attack_Range_Combo: AI_Remark_ModeCombo
+            {
+                REGISTER_FNC;
+
+                idc = PROP_IDC_OFFSET(2022);
+                tooltip = "$STR_BCE_tip_Attack_Range";
+                class Items
+                {
+                    class 2000m
+                    {
+                        text = "2000m";
+                        value = 2000;
+                        default = 1;
+                    };
+                    class 1500m
+                    {
+                        text = "1500m";
+                        value = 1500;
+                    };
+                    class 1000m
+                    {
+                        text = "1000m";
+                        value = 1000;
+                    };
+                };
+            };
+            class Round_Count_Box: RscEdit
+            {
+                REGISTER_FNC;
+
+                idc = PROP_IDC_OFFSET(2023);
+                Style = 2;
+                text = "1";
+                tooltip = "$STR_BCE_tip_Round_Count";
+            };
+            class Attack_Height_Box: Round_Count_Box
+            {
+                REGISTER_FNC;
+
+                idc = PROP_IDC_OFFSET(2024);
+                tooltip = "$STR_BCE_tip_Attack_Height";
+                text = "2000";
+            };
+    
+    //- IP
+        class New_Task_IPtype: RscToolbox
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(2012);
+            
+            shadow = 1;
+            rows = 1;
+            columns = 3;
+            strings[] =
+            {
+                "$STR_BCE_Tit_Map_marker",
+                "$STR_BCE_Tit_Click_Map",
+                "$STR_BCE_Tit_OverHead"
+            };
+            
+            font = "RobotoCondensed_BCE";
+            colorBackground[] = {0,0,0,0.3};
+            onToolBoxSelChanged = "call BCE_fnc_HandleTaskElementChange";
+        };
+        class New_Task_MarkerCombo: AI_Remark_WeaponCombo
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(2013);
+            wholeHeight = 0.8;
+            font = "PuristaMedium";
+            onMouseButtonClick = "(_this # 0) call BCE_fnc_IPMarkers";
+            class Items
+            {
+                class NA
+                {
+                    text = "$STR_BCE_SelectMarker";
+                    data = "[]";
+                    default = 1;
+                };
+            };
+        };
+        class New_Task_IPExpression: RscEdit
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(2014);
+            text = "";
+            canModify = 0;
+            colorBackground[] = {0,0,0,0};
+            tooltip = "$STR_BCE_tip_ShowResult";
+        };
+    
+    //- TG Description
+        class New_Task_TGT: New_Task_IPtype
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(20121);
+            columns = 2;
+            strings[] =
+            {
+                "$STR_BCE_Tit_Map_marker",
+                "$STR_BCE_Tit_Click_Map"
+            };
+        };
+        class New_Task_TG_DESC: RscEdit
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(2015);
+            text = "";
+            tooltip="$STR_BCE_TIP_AddDESC";
+        };
+        class New_Task_TG_DESC_Combo: RscCombo
+        {
+            idc = PROP_IDC_OFFSET(2027);
+            
+            wholeHeight = 0.8;
+            font = "PuristaMedium";
+            tooltip = "$STR_BCE_TIP_DESC";
+            
+            style="0x10 + 0x200 + 0x02";
+            colorText[] = {1,1,1,1};
+            colorBackground[]={0,0,0,0.75};
+            colorSelect[]={1,1,1,1};
+            colorSelectBackground[]={0.2,0.2,0.2,1};
+            
+            class Items
+            {
+                class Custom
+                {
+                    text = "$STR_BCE_DESC_Custom";
+                    textRight = "";
+                    value = 0;
+                    default = 1;
+                };
+                class Open
+                {
+                    text = "$STR_BCE_DESC_Open";
+                    textRight = "";
+                    value = 1;
+                    color[]={0.65,0.65,0.65,1};
+                };
+                class Cover: Open
+                {
+                    text = "$STR_BCE_DESC_Cover";
+                    value = 2;
+                };
+                class treeline: Open
+                {
+                    text = "$STR_BCE_DESC_treeline";
+                    value = 3;
+                };
+                class Hardstructure: Open
+                {
+                    text = "$STR_BCE_DESC_Hardstructure";
+                    value = 4;
+                };
+            };
+        };
+
+    //- Mark
+        class New_Task_GRID_DESC: New_Task_IPExpression
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(2016);
+            canModify = 1;
+            text = "$STR_BCE_MarkWith";
+        };
+        class New_Task_FRND_DESC: New_Task_GRID_DESC
+        {
+            REGISTER_FNC;
+            idc = PROP_IDC_OFFSET(2026);
+        };
+
+    //- ERGS
+        class New_Task_EGRS_Azimuth: RscToolbox
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(2017);
+            rows = 1;
+            columns = 8;
+            strings[] =
+            {
+                "N",
+                "NE",
+                "E",
+                "SE",
+                "S",
+                "SW",
+                "W",
+                "NW"
+            };
+            values[] =
+            {
+                0,
+                45,
+                90,
+                135,
+                180,
+                225,
+                270,
+                315
+            };
+            font = "RobotoCondensed_BCE";
+            colorBackground[] = {0,0,0,0.3};
+        };
+        class New_Task_EGRS_Bearing: New_Task_GRID_DESC
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(2018);
+            text = "$STR_BCE_Bearing_ENT";
+        };
+        class New_Task_EGRS: New_Task_IPtype
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(2019);
+            columns = 4;
+            strings[] =
+            {
+                "$STR_BCE_Tit_Azimuth",
+                "$STR_BCE_Tit_Bearing",
+                "$STR_BCE_Tit_Map_marker",
+                "$STR_BCE_Tit_OverHead"
+            };
+        };
+        
+    //- Remarks
+        class New_Task_FADH: New_Task_IPtype
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(2200);
+            columns = 3;
+            strings[] =
+            {
+                "FAD",
+                "FAH",
+                "$STR_BCE_Default"
+            };
+        };
+        class New_Task_DangerClose_Text: RscText
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(2201);
+            text = ": Danger Close";
+        };
+        class New_Task_DangerClose_Box: RscCheckBox
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(2202);
+            textureChecked = "\a3\3DEN\Data\Controls\ctrlCheckbox\textureChecked_ca.paa";
+            textureFocusedChecked = "\a3\3DEN\Data\Controls\ctrlCheckbox\textureChecked_ca.paa";
+            textureHoverChecked = "\a3\3DEN\Data\Controls\ctrlCheckbox\textureChecked_ca.paa";
+            texturePressedChecked = "\a3\3DEN\Data\Controls\ctrlCheckbox\textureChecked_ca.paa";
+            textureDisabledChecked = "\a3\3DEN\Data\Controls\ctrlCheckbox\textureChecked_ca.paa";
+        };
+    //- CFF
+        class New_Task_CFF_CtrlType: New_Task_IPtype
+        {
+            REGISTER_FNC;
+
+            idc = PROP_IDC_OFFSET(2203);
+            strings[] =
+            {
+                "At-Ready",
+                "AMC",
+                "TOT"
+            };
+            tooltips[] =
+            {
+                "Fire When Ready",
+                "At My Command",
+                """Impact"" Time on Target"
+            };
+        };
+};
+
+#undef PROP_IDC_OFFSET
+#undef REGISTER_FNC
