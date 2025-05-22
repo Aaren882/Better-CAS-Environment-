be79583e76ffe6010abb7f35afdce20d672aa35b
Mike Grubenmann <mgrubenmann@gmail.com>
2025-05-22 12:48:24 +0200

    Readding Greasepencil mark for BCE Point off

--- config.cpp
+++ config.cpp
@@ -285,7 +285,7 @@ class CfgVehicles
 		{
 			class HUD
 			{
-				getin="if (((_this # 0) animationPhase 'Addcrosshair') != 0) then{(_this # 0) animate ['Addcrosshair', 0];};";
+				getin="if (((_this # 0) animationPhase 'Addcrosshair') != 0 && BCE_HUD_fn ) then {(_this # 0) animate ['Addcrosshair', 0];} else {(_this # 0) animate ['Addcrosshair', 1]};";
 			};
 		};
 	};
@@ -316,7 +316,7 @@ class CfgVehicles
 			{
 				class HUD
 				{
-					getin="if (((_this # 0) animationPhase 'Addcrosshair') != 0) then{(_this # 0) animate ['Addcrosshair', 0];};";
+					getin="if (((_this # 0) animationPhase 'Addcrosshair') != 0 && BCE_HUD_fn ) then {(_this # 0) animate ['Addcrosshair', 0];} else {(_this # 0) animate ['Addcrosshair', 1]};";
 				};
 			};
 		};
