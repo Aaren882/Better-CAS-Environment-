4d92803fce02a275838eae60c50def63590f29a6
Aaren <aaren.yang@gmail.com>
2025-07-20 17:42:43 +0800

    Fix cannot found POLPOX MapTools UI elements

--- config.cpp
+++ config.cpp
@@ -783,8 +783,11 @@ class CfgFunctions
 
 			//- Registries
 				class RegisterMissionControls;
-				class onLoad_BCE_Holder;
-				class get_BCE_Holder;
+        //- BCE Interface control Holder
+          class onLoad_BCE_Holder;
+          class get_BCE_Holder;
+          class get_BCE_Holder_Name;
+
 				class get_BCE_curDisplay;
 				class get_Control_Data; //- "BCE_Data"
 				class set_Control_Data; //- "BCE_Data"
@@ -1450,6 +1453,65 @@ class BCE_RscButtonMenu: RscButtonMenu
 		shadow = "false";
 	};
 };
+//-POLPOX Map Tools Controls
+  #if cTAB_Installed == PLP_TOOL
+    class PLP_SMT_Description;
+    class PLP_SMT_Data
+    {
+      class RadialMenu
+      {
+        class Distance
+        {
+          displayName = "$STR_BCE_PLP_Title_Distance";
+          function = "PLP_fnc_SMT_distance";
+          controls = "$STR_BCE_PLP_Ctrl_Distance";
+          description = "$STR_BCE_PLP_Tip_Distance";
+        };
+        class MarkHouses
+        {
+          displayName = "$STR_BCE_PLP_Title_Mark_House";
+          function = "PLP_fnc_SMT_markHouses";
+          controls = "$STR_BCE_PLP_Ctrl_Mark_House";
+          description = "$STR_BCE_PLP_Tip_Mark_House";
+        };
+        class Height
+        {
+          displayName = "$STR_BCE_PLP_Title_Height";
+          function = "PLP_fnc_SMT_height";
+          controls = "$STR_BCE_PLP_Ctrl_Height";
+          description = "$STR_BCE_PLP_Tip_Height";
+        };
+        class Compass
+        {
+          displayName = "$STR_BCE_PLP_Title_Compass";
+          function = "PLP_fnc_SMT_compass";
+          controls = "$STR_BCE_PLP_Ctrl_Compass";
+          description = "$STR_BCE_PLP_Tip_Compass";
+        };
+        class EditGrid
+        {
+          displayName = "$STR_BCE_PLP_Title_Edit_Grid";
+          function = "PLP_fnc_SMT_placeGrid";
+          controls = "$STR_BCE_PLP_Ctrl_Edit_Grid";
+          description = "$STR_BCE_PLP_Tip_Edit_Grid";
+        };
+        class FindFlat
+        {
+          displayName = "$STR_BCE_PLP_Title_Find_Flat";
+          function = "PLP_fnc_SMT_findFlat";
+          controls = "$STR_BCE_PLP_Ctrl_Find_Flat";
+          description = "$STR_BCE_PLP_Tip_Find_Flat";
+        };
+        class LineOfSight
+        {
+          displayName = "$STR_BCE_PLP_Title_Line_of_Sight";
+          function = "PLP_fnc_SMT_lineOfSight";
+          controls = "$STR_BCE_PLP_Ctrl_Line_of_Sight";
+          description = "$STR_BCE_PLP_Tip_Line_of_Sight";
+        };
+      };
+    };
+  #endif
 
 //- Mission Property + Controls + Map Infos
 	#include "Mission_Map_Infos.hpp"
@@ -1496,66 +1558,6 @@ class BCE_RscButtonMenu: RscButtonMenu
 //UI
   #include "Control_UI.hpp"
   #include "Dialog.hpp"
-
-//-POLPOX Map Tools Control
-#if cTAB_Installed == PLP_TOOL
-	class PLP_SMT_Description;
-	class PLP_SMT_Data
-	{
-		class RadialMenu
-		{
-			class Distance
-			{
-				displayName = "$STR_BCE_PLP_Title_Distance";
-				function = "PLP_fnc_SMT_distance";
-				controls = "$STR_BCE_PLP_Ctrl_Distance";
-				description = "$STR_BCE_PLP_Tip_Distance";
-			};
-			class MarkHouses
-			{
-				displayName = "$STR_BCE_PLP_Title_Mark_House";
-				function = "PLP_fnc_SMT_markHouses";
-				controls = "$STR_BCE_PLP_Ctrl_Mark_House";
-				description = "$STR_BCE_PLP_Tip_Mark_House";
-			};
-			class Height
-			{
-				displayName = "$STR_BCE_PLP_Title_Height";
-				function = "PLP_fnc_SMT_height";
-				controls = "$STR_BCE_PLP_Ctrl_Height";
-				description = "$STR_BCE_PLP_Tip_Height";
-			};
-			class Compass
-			{
-				displayName = "$STR_BCE_PLP_Title_Compass";
-				function = "PLP_fnc_SMT_compass";
-				controls = "$STR_BCE_PLP_Ctrl_Compass";
-				description = "$STR_BCE_PLP_Tip_Compass";
-			};
-			class EditGrid
-			{
-				displayName = "$STR_BCE_PLP_Title_Edit_Grid";
-				function = "PLP_fnc_SMT_placeGrid";
-				controls = "$STR_BCE_PLP_Ctrl_Edit_Grid";
-				description = "$STR_BCE_PLP_Tip_Edit_Grid";
-			};
-			class FindFlat
-			{
-				displayName = "$STR_BCE_PLP_Title_Find_Flat";
-				function = "PLP_fnc_SMT_findFlat";
-				controls = "$STR_BCE_PLP_Ctrl_Find_Flat";
-				description = "$STR_BCE_PLP_Tip_Find_Flat";
-			};
-			class LineOfSight
-			{
-				displayName = "$STR_BCE_PLP_Title_Line_of_Sight";
-				function = "PLP_fnc_SMT_lineOfSight";
-				controls = "$STR_BCE_PLP_Ctrl_Line_of_Sight";
-				description = "$STR_BCE_PLP_Tip_Line_of_Sight";
-			};
-		};
-	};
-#endif
 
 class CfgFontFamilies
 {
