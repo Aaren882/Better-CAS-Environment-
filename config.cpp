b40cf8dbdc401a7c938bab398b1b03ee9f4f6c6a
Aaren <aaren.yang@gmail.com>
2025-09-02 16:34:12 +0800

    Add Greasepencil mark for vanilla AH-6 when BCE Aimpoint is deactivated

--- config.cpp
+++ config.cpp
@@ -259,14 +259,21 @@ class CfgVehicles
 			class MainTurret;
 		};
 	};
-	class Heli_Light_01_base_F;
+	class Heli_Light_01_base_F: Helicopter_Base_H {};
 	class Heli_Light_01_armed_base_F: Heli_Light_01_base_F
 	{
-		hiddenSelectionsTextures[] = {"A3\Air_F\Heli_Light_01\Data\Heli_Light_01_ext_Blufor_CO.paa"};
+		// hiddenSelectionsTextures[] = {"A3\Air_F\Heli_Light_01\Data\Heli_Light_01_ext_Blufor_CO.paa"};
 		defaultUserMFDvalues[]={0.15,1,0.15,0.7};
 		class MFD
 		{
 			#include "MFD\HUD.hpp"
+		};
+		class EventHandlers: EventHandlers
+		{
+			class HUD
+			{
+				getin="(_this # 0) setObjectTexture [1, ['a3\air_f\heli_light_01\data\heli_light_01_dot_ca.paa',''] select BCE_HUD_fn];";
+			};
 		};
 	};
 	//MELB
