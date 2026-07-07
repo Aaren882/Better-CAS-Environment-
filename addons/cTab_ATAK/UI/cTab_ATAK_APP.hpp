cb0ee1d732772b723dd6c42bfc01d1b4569f5bc2
Aaren <aaren.yang@gmail.com>
2026-07-08 02:10:23 +0800

    Migrate `settings` app to dedicated addon (not functioning)

--- addons/cTab_ATAK/UI/cTab_ATAK_APP.hpp
+++ addons/cTab_ATAK/UI/cTab_ATAK_APP.hpp
@@ -1,19 +1,1 @@
 #include "ATAK_UI_Base.hpp"
-// #include "ATAK_Buttons.hpp"
-
-class ATAK_APPs
-{
-  //-Thired Line
-    
-    class settings: BCE_ATAK_Tool_ICON
-    {
-      class Menu_Property
-      {
-        ORDER = 8;
-        PAGE_CTRL = "";
-      };
-
-      text = ATAK_APP(Settings);
-			textureNoShortcut = QPATHTOEF(Core,data\settings.paa);
-    };
-};
