55231920c0898021a7d01c6e880829f8d01a485c
Aaren <aaren.yang@gmail.com>
2026-07-16 18:26:30 +0800

    Add `TaskViewer` interface prototype (WIP)

--- addons/cTab_ATAK_taskViewer/Configs/page.hpp
+++ addons/cTab_ATAK_taskViewer/Configs/page.hpp
@@ -1,3 +1,29 @@
 class ATAK_AppMenu_Base;
 
-//- Interface below
+//- Message Interface
+	class ATAK_Message: ATAK_AppMenu_Base
+	{
+		class controls
+		{
+			class Title;
+			class Group_Box;
+		};
+	};
+//- Group Manage
+  class ATAK_TaskViewer: ATAK_Message
+  {
+    class controls: controls
+    {
+      class Title: Title
+      {
+        idc = 5;
+        text = "Tasks";
+      };
+
+      class Group_Box: Group_Box
+      {
+        idc = 10;
+        h = QUOTE(phoneSizeH - 1.55 * ATAK_POS_H);
+      };
+    };
+  };
